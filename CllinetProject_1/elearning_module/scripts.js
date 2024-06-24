8198d0b190f0d8564d1dee1debb674ca0bd263df
everettstuckey <everett.stuckey@outlook.com>
2024-06-24 01:57:20 -0500

    Add files via upload

--- CllinetProject_1/elearning_module/scripts.js
+++ CllinetProject_1/elearning_module/scripts.js
@@ -12,4 +12,16 @@ function changeState(state) {
         waterImage.alt = 'Boiling Water';
     }
 }
-    
+function speakText(text) {
+    const speechSynthesis = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.rate = 1; // Speed of the speech
+    utterance.pitch = 1; // Pitch of the speech
+    utterance.volume = 1; // Volume of the speech
+    speechSynthesis.speak(utterance);
+}
+
+function narratePage() {
+    const pageContent = document.querySelector('main').innerText;
+    speakText(pageContent);
+}
